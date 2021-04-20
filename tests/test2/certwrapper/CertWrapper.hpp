9c9e24326f8446a77a95e4ec53d80d73406f68e6
heesoon <chipmaker0304@gmail.com>
2021-04-20 01:21:53 +0000

    add cert codes

--- tests/test2/certwrapper/CertWrapper.hpp
+++ tests/test2/certwrapper/CertWrapper.hpp
@@ -1,12 +1,17 @@
 #include "bioWrapper.hpp"
 #include <string>
-#include <openssl/conf.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
 
 class CertWrapper
 {
 public:
     CertWrapper();
+    //bool openCert(const std::string &inputFileName, int format);
+    bool readCert(const std::string &inputFileName, int format);
+    bool writeCert(const std::string &outputFileName, int format);
+    X509* getX509();
     virtual ~CertWrapper();
 private:
-
+    X509 *x509;
 };
--- tests/test2/certwrapper/CertWrapper_test.cpp
+++ tests/test2/certwrapper/CertWrapper_test.cpp
@@ -5,7 +5,9 @@
 
 void testCert()
 {
-
+	std::string input_certificate_filename = "customer_certificate.pem";
+	std::unique_ptr<CertWrapper> upCertWrapper(new CertWrapper);
+	upCertWrapper->readCert(input_certificate_filename, FORMAT_PEM);
 }
 
 int main()
