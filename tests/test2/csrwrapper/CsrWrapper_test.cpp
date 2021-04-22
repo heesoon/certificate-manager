a4eef79258c2e5c4b3d2308d6f406a86672044e8
heesoon <chipmaker0304@gmail.com>
2021-04-22 01:08:37 +0000

    add temporal

--- tests/test2/csrwrapper/CsrWrapper_test.cpp
+++ tests/test2/csrwrapper/CsrWrapper_test.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include "bioWrapper.hpp"
 #include "CsrWrapper.hpp"
 
-void testCsr()
+void testGenerateCsr()
 {
+	bool ret = false;
+	CsrWrapper cswrapper;
 
+	std::string input_config_filename = "/home/hskim/share/certificate-manager/tests/test3/scripts/customer_openssl.cnf";
+	std::string input_privatekey_filename = "test_key.pem";
+	std::string output_csr_filename = "csr.pem";
+
+	subject_t subject;
+	subject.commonName = "Customer Inc";
+	subject.countryName = "KR";
+	subject.stateOrProvinceName = "Seoul";
+	subject.localityName = "Seoul";
+	subject.organizationName = "Customer Inc R&D";
+	subject.emailAddress = "customer@rnd.com";
+
+	ret = cswrapper.makeCsr(input_privatekey_filename, input_config_filename, subject);
+	if(ret == false)
+	{
+		return;
+	}
+
+	ret = cswrapper.writeCsr(output_csr_filename, FORMAT_PEM);
+	if(ret == false)
+	{
+		return;
+	}
+
+	std::cout << "Success" << std::endl;
 }
 
 int main()
 {
-	testCsr();
+	testGenerateCsr();
 	return 0;
 }
