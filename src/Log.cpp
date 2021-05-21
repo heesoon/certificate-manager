b4be8226d2ec39624ddbf302c58e8b24979fd03f
heesoon.kim <chipmaker0304@gmail.com>
2021-05-21 04:02:35 +0000

    modify log code

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,20 +1,27 @@
+// Copyright (c) 2013-2018 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "Log.hpp"
 
-void PmLogMsg(const char* logContext, const char *fmt, ...)
+extern PmLogContext GetCertificateManagerPmLogContext()
 {
-    int n;
-    char buf[MAX_BUFF_LENGTH] = {0, };
-
-    va_list args;
-    va_start(args, fmt);
-    //va_copy(args2, args1);
-    n = vsnprintf(buf, sizeof(buf), fmt, args);
-    va_end(args);
-
-    if(n > (sizeof(buf) -2))
+    static PmLogContext certificateManagerLogContext = 0;
+    if (0 == certificateManagerLogContext)
     {
-        return;
+        PmLogGetContext("CERTIFICATE_MANAGER", &certificateManagerLogContext);
     }
-
-    printf("[%s]: %s\n", logContext, buf);	
+    return certificateManagerLogContext;
 }
